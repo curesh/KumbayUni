cf839adaaad24fbe599b09f7fba568184ccf8521
Chandra Suresh <chandra.b.suresh@gmail.com>
2020-08-25 23:48:47 -0700

    Work on backend video uploading

--- database/init_db.py
+++ database/init_db.py
@@ -2,9 +2,10 @@ import sqlite3
 
 connection = sqlite3.connect('database.db')
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
 
+with open('./schema.sql') as f:
+    connection.executescript(f.read())
+    
 cur = connection.cursor()
 cur.execute("INSERT INTO links (title, link_hash) VALUES (?, ?)",
             ('Physics MIT  Lecture 1', 'wWnfJ0-xXRE')
@@ -14,12 +15,12 @@ cur.execute("INSERT INTO links (title, link_hash) VALUES (?, ?)",
             ('Physics MIT Lecture 2', 'GtOGurrUPmQ')
             )
 
-cur.execute("INSERT INTO lectures (title, file_name) VALUES (?, ?)",
-            ("Video Test 1", "test1.mp4")
+cur.execute("INSERT INTO lectures (file_name) VALUES (?)",
+            ("test1.mp4",)
             )
 
-cur.execute("INSERT INTO lectures (title, file_name) VALUES (?, ?)",
-            ("Video Test 2", "test2.mp4")
+cur.execute("INSERT INTO lectures (file_name) VALUES (?)",
+            ("test2.mp4",)
             )
 
 connection.commit()
--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
+import os
 
 def get_db_connection():
     conn = sqlite3.connect('database/database.db')
@@ -9,6 +10,7 @@ def get_db_connection():
     return conn
 
 app = Flask(__name__)
+#app.run(host=HOST, port=PORT, threaded=True)
 app.config['SECRET_KEY'] = 'fjdalfiipyujsal7f34ks7a13lfnfa441ksivnor231'
 
 @app.route('/')
@@ -32,24 +34,47 @@ def link(link_id):
     link = get_link(link_id)
     return render_template('link.html', link=link)
 
-app.config['UPLOAD_FOLDER'] = "assets/test_data/vids"
+app.config['UPLOAD_FOLDER'] = "/Users/bigboi01/Documents/CSProjects/kumbayuni/assets/test_data/vids"
 # Maximum upload size is 100 mB
 app.config['MAX_CONTENT_PATH'] = 100000000
+app.config['ALLOWED_VID_EXTENSIONS'] = ["MP4", "MKV", "MOV", "WMV", "AVI"]
+
+def allowed_images(filename):
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config['ALLOWED_VID_EXTENSIONS']:
+        return True
+    else:
+        return False
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         f = request.files['file']
+
+        print("f ", f)
+
         if not f:
             flash('File is required')
+
         title = request.form['title']
+        full_file = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)) 
         if not title:
             flash('Title is required!')
+        elif f.filename == "":
+            flash('Video must have filename')
+        elif not allowed_images(f.filename):
+            flash('That video extension is not allowed')
+        elif os.stat(full_file).st_size > app.config['MAX_CONTENT_PATH']:
+            flash('Video is too large. Please, split into two videos.')
         else:
-            f.save(secure_filename(f.filename))
+            f.save(full_file)
             conn = get_db_connection()
-            conn.execute('INSERT INTO lectures (title, file) VALUES (?, ?)',
-                         (title, f.filename))
+            conn.execute('INSERT INTO lectures (file_name) VALUES (?)',
+                         (f.filename,))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
